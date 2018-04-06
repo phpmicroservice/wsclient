d487e04031e5c09e33692c9450b07e7c08097b66
Dongasai <1514582970@qq.com>
2018-04-06 15:38:55 +0800

    2018年4月6日15:38:36

--- start/start.php
+++ start/start.php
@@ -1,7 +1,7 @@
 <?php
 //include './logo.php';
 echo "开始主程序! \n";
-define("SERVICE_NAME", "CONFIG");# 设置服务名字
+define("SERVICE_NAME", "PROXY");# 设置服务名字
 define('ROOT_DIR', dirname(__DIR__));
 require ROOT_DIR . '/vendor/autoload.php';
 # 进行一些项目配置
